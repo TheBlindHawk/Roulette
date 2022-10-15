2ce9542e6152e4312459422a44077ac3e47b7c61
Leo Tomiselli <leotomiselli@gmail.com>
2022-10-15 22:57:47 +0900

    version 1.1.0

--- README.md
+++ README.md
@@ -5,10 +5,16 @@ NPM package installation
 npm install theblindhawk/roulette
 ```
 
+## 1.1 Version Updates
+
+1. changes/fix to export method
+2. Roulette() now requires the id of the div
+3. customizable colors!
+
 ## Usage
-create an html div with ```id=roulette-container```
+create an html div with ```id=roulette```
 ```
-<div class="my_class_name" id="roulette-container"></div>
+<div class="my_class_name" id="roulette"></div>
 ```
 
 import and set up the roulette using js
@@ -17,10 +23,38 @@ import { Roulette } from "/js/hawk_roulette.js";
 
 // the array with all the roulette options
 const rolls = [0, 8, 3, 5, 50];
-var roulette = new Roulette(rolls);
+var roulette = new Roulette("roulette", rolls);
 // tell the roulette to roll on said option
 roulette.roll(8);
 ```
+
+
+## Roulette()
+
+```
+new Roulette(roulette_id, rolls, colors = [], width = 310, height = 310, shrink = 20)
+```
+
+**roulette_id**
+type: string
+The id of the div element that will contain the roulette.
+
+**rolls**
+type: array
+The values of each section of the roulette.
+
+**colors**
+type: array
+The colors of each section of the roulette.
+You can input less colors than the values, to have the same colors alternating.
+
+**width, height**
+type: numeric
+the width and height of the roulette element
+
+**shrink**
+type: numeric
+Will shrink the size of the roulette board in comarison to the overall
 
 ## Customization
 
@@ -33,8 +67,6 @@ roulette.roll(8);
 | setSize()     | width, height, *shrink | 310, 310, 20   |
 | setBorder()   | color, width           | #808C94, 10    |
 | addRollText() | *before, *after        | '', ''         |
-
-NB: shrink will shrink the roulette circle drawing compared to the overall
 
 </br>
 
--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-import d3 from 'd3';
+import * as d3 from 'd3';
 
 export class Roulette {
-    #rolling = false;
-    #rotation = 0;
+    #width = 310; #height = 310; #shrink = 20;
+    #rolls = []; #colors = [];
+    #rolling = false; #rotation = 0;
     #addtxt = { before: '', after: '' };
     #border = { color: '#808C94', width: 10 };
+    #roulette_id = 'roulette';
     last_roll = null;
     min_spins = 5;
     audio_dir = '/sounds/soft_click_1s.wav';
     onstop = function() {};
 
-    constructor(rolls, width = 310, height = 310, shrink = 20) {
+    constructor(roulette_id, rolls, colors = [], width = 310, height = 310, shrink = 20) {
+        this.roulette_id = roulette_id;
         this.#width = width;
-        this.#height = height;
+        this.#height = height;  
         this.#shrink = shrink;
         this.#rolls = rolls;
+        this.#colors = colors;
         this.draw();
     }
 
@@ -54,7 +58,7 @@ export class Roulette {
             var slow = Math.min(5, Math.floor((sprint - rotation)/180 * 5));
             var increase = Math.floor((sprint - rotation)/sprint * 10) + slow + 1;
             rotation += increase; audio_counter += increase;
-            document.getElementById('roulette').style.transform = 'rotate(-'+(rotation%360)+'deg)';
+            document.getElementById('roulette-circle').style.transform = 'rotate(-'+(rotation%360)+'deg)';
             if(audio_counter >= audio_distance) {
                 var audio = new Audio(self.audio_dir);
                 audio_counter -= audio_distance;
@@ -78,40 +82,41 @@ export class Roulette {
         this.rollByIndex(indexes[random]);
     }
 
+    #getSector(r, p, s, i) {
+        const a1 = 360 / s * i - 90;
+        const a2 = 360 / s * (i + 1) - 90;
+        const degtorad = Math.PI / 180;
+        const cx1 = Math.cos(degtorad * a1) * r + r + p;
+        const cy1 = Math.sin(degtorad * a1) * r + r + p;
+        const cx2 = Math.cos(degtorad * a2) * r + r + p;
+        const cy2 = Math.sin(degtorad * a2) * r + r + p;
+
+        return `M${r+p} ${r+p} ${cx1} ${cy1} A${r} ${r} 0 0 1 ${cx2} ${cy2}Z`;
+    };
+
     draw() {
-        const container = document.getElementById('roulette-container');
+        const container = document.getElementById(this.#roulette_id);
         container.replaceChildren();
         const roulette = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
         const down_arrow = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
         roulette.setAttribute('width', this.#width); roulette.setAttribute('height', this.#height);
         down_arrow.setAttribute('width', this.#width); down_arrow.setAttribute('height', this.#height);
         down_arrow.style = "position: absolute; left: 50%; transform: translate(-50%,0); z-index: 1;"
-        roulette.id = 'roulette'; down_arrow.id = 'roulette-arrow';
+        roulette.id = 'roulette-circle'; down_arrow.id = 'roulette-arrow';
         container.append(roulette); container.append(down_arrow); 
 
         const sections = this.#rolls.length;
-        const svg = d3.select('#roulette');
+        const svg = d3.select('#roulette-circle');
         const padding = this.#shrink / 2;
-        const radius = (Math.min(this.#width, this.#height) - padding*2) / 2;
+        const radius = (Math.min(this.#width, this.#height) - this.#shrink) / 2;
         const angle = Math.PI * 2 / sections;
         const rotation = 360 / sections;
 
-        // draw the circle
-        svg.append('circle')
-            .attr('cx', radius + padding)
-            .attr('cy', radius + padding)
-            .attr('r', radius - this.#border.width / 2)
-            .attr('fill', 'none')
-            .attr('stroke', this.#border.color)
-            .style('stroke-width', this.#border.width);
-
-        // split the circle
         for (var i = 0; i < sections; i++) {
-            const lx = radius * Math.sin(angle * i) + padding;
-            const ly = radius * -Math.cos(angle * i) + padding;
-            svg.append('line')
-                .attr('x1', radius + padding).attr('y1', radius + padding)
-                .attr('x2', radius + lx).attr('y2', radius + ly)
+            var color = this.#colors.length > 0 ? this.#colors[i % this.#colors.length] : '#fff';
+            svg.append('path')
+                .attr('d', this.#getSector(radius, padding, sections, i))
+                .style('fill', color)
                 .style('stroke', this.#border.color)
                 .style('stroke-width', this.#border.width);
 
@@ -126,10 +131,10 @@ export class Roulette {
                 .text(this.#addtxt.before + this.#rolls[i] + this.#addtxt.after);
         }
 
+        // draw the arrow
         const p1 = (radius)+',0 ';
         const p2 = (radius+padding*2)+',0 ';
         const p3 = radius+padding+','+this.#shrink*2+' ';
-        // draw the arrow
         const arrow_svg = d3.select('#roulette-arrow');
         arrow_svg.append('polygon')
             .attr('points', p1 + p2 + p3)
